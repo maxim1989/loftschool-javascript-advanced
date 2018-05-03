edc14ca3726a63d074d0a56b27750ef2c53a8fef
Fedorov Max <fedorov-max89@mail.ru>
2018-05-03 22:55:57 +0300

    Homework 3 has been completed

--- src/index.js
+++ src/index.js
@@ -17,6 +17,19 @@
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
 function isAllTrue(array, fn) {
+    if (!Array.isArray(array) || array.length === 0) {
+        throw new Error('empty array');
+    }
+    if (typeof fn !== 'function') {
+        throw new Error('fn is not a function');
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (!fn(array[i])) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 /*
@@ -36,6 +49,19 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
+    if (!Array.isArray(array) || array.length === 0) {
+        throw new Error('empty array');
+    }
+    if (typeof fn !== 'function') {
+        throw new Error('fn is not a function');
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (fn(array[i])) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 /*
@@ -49,7 +75,18 @@ function isSomeTrue(array, fn) {
  3.3: Необходимо выбрасывать исключение в случаях:
    - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn) {
+function returnBadArguments(fn, ...restArgs) {
+    if (typeof fn !== 'function') {
+        throw new Error('fn is not a function');
+    }
+
+    return restArgs.filter(item => {
+        try {
+            fn(item);
+        } catch (e) {
+            return true;
+        }
+    });
 }
 
 /*
@@ -69,7 +106,51 @@ function returnBadArguments(fn) {
    - number не является числом (с текстом "number is not a number")
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator() {
+function calculator(number=0) {
+    if (!Number.isFinite(number)) {
+        throw new Error('number is not a number');
+    }
+
+    return {
+        sum(...rest) {
+            rest.forEach(item => {
+                number += item;
+            });
+
+            return number;
+        },
+
+        dif(...rest) {
+            rest.forEach(item => {
+                number -= item;
+            });
+
+            return number;
+        },
+
+        div(...rest) {
+            rest.forEach(item => {
+                if (item === 0) {
+                    throw new Error('division by 0');
+                }
+            });
+
+            rest.forEach(item => {
+                number /= item;
+            });
+
+            return number;
+
+        },
+
+        mul(...rest) {
+            rest.forEach(item => {
+                number *= item;
+            });
+
+            return number;
+        }
+    }
 }
 
 /* При решении задач, пострайтесь использовать отладчик */
