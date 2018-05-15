4ca1fc6a47aab1607019b712748794c61ba2d4f9
Fedorov Max <fedorov-max89@mail.ru>
2018-05-15 22:53:13 +0300

    hw6 completed

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
 function delayPromise(seconds) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, seconds*1000);
+    });
 }
 
 /*
@@ -25,6 +30,31 @@ function delayPromise(seconds) {
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
 function loadAndSortTowns() {
+    const xhr = new XMLHttpRequest(),
+        url = 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json';
+
+    xhr.open('GET', url, true);
+
+    return new Promise((resolve, reject) => {
+        xhr.onload = function() {
+            resolve(JSON.parse(xhr.responseText).sort(
+                function (a, b) {
+                    if (a.name > b.name) {
+                        return 1;
+                    }
+                    if (a.name < b.name) {
+                        return -1;
+                    }
+
+                    return 0;
+                }
+            ));
+        };
+        xhr.onerror = function(error) {
+            reject(error);
+        }
+        xhr.send();
+    })
 }
 
 export {
